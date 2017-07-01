cfdf5b598a96b949464439a8228c6acbb5fc9894
DanzisAlf <migalha_fei@hotmail.com>
2017-07-01 17:00:11 -0300

    Exercicio IDE package 7.6

--- fj11-contas/src/br/com/caelum/contas/main/TestaConta.java
+++ fj11-contas/src/br/com/caelum/contas/main/TestaConta.java
@@ -1,9 +1,12 @@
+package br.com.caelum.contas.main;
+import br.com.caelum.contas.modelo.Conta;
 
 public class TestaConta {
 	public static void main(String[] args) {
 		Conta conta = new Conta();
 		conta.setTitular("Ozzy");
 		conta.deposita(300.0);
+
 		if (conta.saca(500.0)) {
 			System.out.println("Sacou:");
 		} else {
