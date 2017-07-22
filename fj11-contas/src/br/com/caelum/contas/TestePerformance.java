552f8cd36f2ce945029577cf0c0507321905656d
DanzisAlf <migalha_fei@hotmail.com>
2017-07-22 17:02:42 -0300

    exercicio 16.11

--- fj11-contas/src/br/com/caelum/contas/TestePerformance.java
+++ fj11-contas/src/br/com/caelum/contas/TestePerformance.java
@@ -1,26 +1,32 @@
 package br.com.caelum.contas;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+//ArrayList adiciona mais rapido, enquanto HashSet busca mais rapido
 
 public class TestePerformance {
 	public static void main(String[] args) {
-		System.out.println("Iniciando...");
 
-		Collection<Integer> teste = new ArrayList<>();
-		long inicio = System.currentTimeMillis();
+		Collection<Integer> teste = new HashSet<>();
+
+		System.out.println("Iniciando...");
 		int total = 30000;
 
+		long inicio = System.currentTimeMillis();
 		for (int i = 0; i < total; i++) {
 			teste.add(i);
 		}
-		
-		for (int i = 0; i < total ; i ++){
-			teste.contains(i);
-		}
-		
 		long fim = System.currentTimeMillis();
 		long tempo = fim - inicio;
-		System.out.println("total gasto " + tempo);
+		System.out.println("adicao... " + tempo);
+
+		inicio = System.currentTimeMillis();
+		for (int i = 0; i < total; i++) {
+			teste.contains(i);
+		}
+		fim = System.currentTimeMillis();
+		tempo = fim - inicio;
+		System.out.println("leitura... " + tempo);
+
 	}
 }
